4f2412f1a55e3e27110b9e255c07bb1ff04bfed1
Boanerghes <palmiami@dei.unipd.it>
2012-12-28 11:52:49 +0100

    changed svm training method

--- js/twitStream.js
+++ js/twitStream.js
@@ -4,18 +4,21 @@ function initSVM() {
 	console.log("SVM initialized");
 }
 
+// main function called when clicking the search button
 function evaluateAndDisplay (settings) {
 	var coll = new TweetCollection();
 	coll.fetch(settings.keyword, settings.num, 'en',  coll.displayWithSentiment(settings.elem));
 }
 
+// return a function that gets a json representation of tweets and prints them nicely in the element 'elem'
 TweetCollection.prototype.displayWithSentiment = function (elem){
 	return function (json) {
 		elem = $(elem);
-		
+
 		elem.html('');
 		$(json.results).each(function(){
 			var actualTweet = this.text.delinkify().removeUsers().removeHash().toLowerCase();
+			// create boolean vector and print result
 			var res = BOW.getPresence(actualTweet.toBOW());
 			if (res.sum != 0){
 				var mood = (svm.predict([res.result]) == 1) ? "POSITIVE" : "NEGATIVE";
@@ -61,9 +64,9 @@ TweetCollection.displayTweets = function(elem){
 	}
 }
 
-// Transform a vector in a boolean presence array given an array of elements. The output array has the same 
-// length of the bag of elements, subject of this procedure, and each of its elements reflects the presence of that particular element
-// in the original vector.
+//Transform a vector in a boolean presence array given an array of elements. The output array has the same 
+//length of the bag of elements, subject of this procedure, and each of its elements reflects the presence of that particular element
+//in the original vector.
 Array.prototype.getPresence = function(elements) {
 
 	var output=new Array();
@@ -106,7 +109,7 @@ String.prototype.removeHash = function(){
 };
 
 
-// Return the indexes of true boolean elements in an array.
+//Return the indexes of true boolean elements in an array.
 Array.prototype.findTrueIndexes = function() {
 	var output = new Array();
 
@@ -117,15 +120,16 @@ Array.prototype.findTrueIndexes = function() {
 	return output;
 }
 
+
+// various methods for training the SVM
+
 function setUpAndTest() {
 	posVector = new Array();
 	negVector = new Array();
 	var i = 0;
-
 	this.tweetsToVector = function(tweet_coll, array) {
 		$(tweet_coll.results).each( function(){ 
 			var actualTweet = this.text.delinkify().removeUsers().removeHash().toLowerCase();
-
 			var arr = actualTweet.toBOW();
 			var res = BOW.getPresence(arr);
 			if (res.sum != 0){ 
@@ -135,7 +139,6 @@ function setUpAndTest() {
 			else console.log("REJECTED " + actualTweet);
 		});	
 	}
-
 	this.recursiveFetch = function(keyword, array, max) {
 		console.log("recursive call");
 		var coll = new TweetCollection();
@@ -161,8 +164,95 @@ function setUpAndTest() {
 		}
 		svm = new svmjs.SVM();
 		svm.train(data, labels, {C: 1.0});
-		
+
 		console.log(JSON.stringify(svm.toJSON()));
 	}
 
+}
+
+
+function calculateRecurrence() {
+	this.tweetsToWordsCount = function(tweet_coll, destArray) {
+		$(tweet_coll.results).each( function(){ 
+
+			var actualTweet = this.text.delinkify().removeUsers().toLowerCase();
+			destArray.push(actualTweet);
+
+			var arr = actualTweet.toBOW();
+			for ( var i =0; i<arr.length; i++) {
+				var token = arr[i];
+				if (words[token] == undefined) { words[token] = 1; }
+				else { words[token] ++; }
+			}
+		});	
+	}
+
+	exec = false;
+	this.recursiveFetch = function(keyword, destArray, max) {
+		console.log("recursive call " + keyword);
+		var coll = new TweetCollection();
+		coll.fetch(keyword, 100, "en", function(json){
+			tweetsToWordsCount(json, destArray);
+			console.log ("ended recursive fetch - " + destArray.length);
+			if (destArray.length <= max){
+				recursiveFetch(keyword, destArray, max);
+			}
+			else {
+				if (!exec){ exec = true;}
+				else if (exec) {
+					console.log("gone");
+					trainSVM();
+				}
+			}
+		});
+	}
+
+	this.trainSVM = function () {
+		var sortable = [];
+
+		for (token in words) {
+			if (words.hasOwnProperty(token)){
+				sortable.push([token, words[token]]);
+			}
+		}
+		sortable.sort( function (a, b) {
+			return b[1] - a[1];
+		});
+
+		newBOW = [];
+		// update here for bigger BOW
+		for (var i=0; i<200; i++) {
+			newBOW.push(sortable[i][0]);
+		}
+
+		svm = new svmjs.SVM();
+		labels = [];
+		vectors = [];
+		j = 0;
+
+		for (var i=0; i<posTweets.length; i++){
+			var vec = newBOW.getPresence(posTweets[i].toBOW());
+			if (vec.sum != 0) {
+				vectors[j] = vec.result;
+				labels[j++] = 1;		
+			}
+		}
+		for (var i=0; i<negTweets.length; i++){
+			var vec = newBOW.getPresence(negTweets[i].toBOW());
+			if (vec.sum != 0) {
+				vectors[j] = vec.result;
+				labels[j++] = -1;		
+			}
+		}
+		console.log(j);
+		svm.train(vectors, labels, {C: 1.0});
+		console.log(JSON.stringify(svm.toJSON()));
+	}
+
+	var words = new Array();
+	posTweets = [];
+	negTweets = [];
+
+	recursiveFetch(":) OR :D OR :-) OR (: OR (-:", posTweets, 2000);
+	recursiveFetch(":( OR ): OR :-( OR )-:", negTweets, 2000);
 }
